c3905dc8eb055d4d891e32eb8e681a81af917887
SonBui234 <70386074+SonBui234@users.noreply.github.com>
2021-04-19 15:26:33 +0700

    env-file (#25)
    Co-authored-by: son.bui@amela.vn <son.bui@amela.vn>

--- src/app/helpers/twillio.ts
+++ src/app/helpers/twillio.ts
@@ -1,6 +1,6 @@
 import config from "$config";
 import format from "string-format";
-// const client = require("twilio")(config.twilio.sid, config.twilio.token);
+const client = require("twilio")(config.twilio.sid, config.twilio.token);
 
 const message = `Mã xác thực của bạn là: {code}`;
 
@@ -10,13 +10,13 @@ export const handlePhoneNumber = async (phoneNumber: string) => {
   return phoneNumber;
 };
 
-// export const sendSMS = async (params: any) => {
-//   const { code, to } = params;
+export const sendSMS = async (params: any) => {
+  const { code, to } = params;
 
-//   const body = format(message, { code });
+  const body = format(message, { code });
 
-//   client.messages
-//     .create({ body, from: config.twilio.phoneNumber, to })
-//     .then(() => console.log("success"))
-//     .catch((err) => console.log("err", err));
-// };
+  client.messages
+    .create({ body, from: config.twilio.phoneNumber, to })
+    .then(() => console.log("success"))
+    .catch((err) => console.log("err", err));
+};
