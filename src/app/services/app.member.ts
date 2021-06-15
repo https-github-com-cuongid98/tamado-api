36304ff296e966a6f442ddf68489622a59fad31b
son.bui@amela.vn <son.bui@amela.vn>
2021-06-15 14:46:48 +0700

    update-notification-follow

--- src/app/services/app.member.ts
+++ src/app/services/app.member.ts
@@ -188,23 +188,23 @@ export async function followMember(memberId: number, targetId: number) {
       memberFollow
     );
 
-    // const notificationObj = new Notification();
-    // notificationObj.memberId = targetId;
-    // notificationObj.redirectId = memberId;
-    // notificationObj.redirectType = RedirectType.MEMBER;
+    const notificationObj = new Notification();
+    notificationObj.memberId = targetId;
+    notificationObj.redirectId = memberId;
+    notificationObj.redirectType = RedirectType.MEMBER;
 
     if (checkMemberFollow) {
       await memberFollowRepository.delete(memberFollow);
-      // notificationObj.content = `${member.memberDetail.name} unfollowed you!`;
+      notificationObj.content = `${member.memberDetail.name} unfollowed you!`;
     } else {
       if (target.status != CommonStatus.ACTIVE) throw ErrorCode.Member_Blocked;
       await memberFollowRepository.save(memberFollow);
-      // notificationObj.content = `${member.memberDetail.name} followed you!`;
+      notificationObj.content = `${member.memberDetail.name} followed you!`;
     }
 
-    // await notificationRepository.save(notificationObj);
+    await notificationRepository.save(notificationObj);
     if (target.receiveNotification == CommonStatus.ACTIVE) {
-      // await pushNotificationToMember(notificationObj);
+      await pushNotificationToMember(notificationObj);
     }
   });
 }
